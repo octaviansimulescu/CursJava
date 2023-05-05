cbe9bf6ba1df6cc74167cc816c6964dc2ef59302
unknown <you@example.com>
2023-05-05 18:02:32 +0300

    curs1

---
+++ src/Curs1/Tester.java
@@ -0,0 +1,21 @@
+package Curs1;
+
+public class Tester {
+
+    private String nume;
+    private int ratePerHour;
+
+    public String getNume(){
+        return nume;
+    }
+
+    public void setNume(String nume){
+        this.nume=nume;
+    }
+
+    /*
+
+     */
+    Tester obj = new Tester();
+
+}
